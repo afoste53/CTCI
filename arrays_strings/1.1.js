91a180fb17961ad5b406fe19ff8834f91d31db18
Gabe <mrginch@gmail.com>
2021-08-19 13:36:36 -0400

    exercise 1.1

---
+++ arrays_strings/1.1.js
@@ -0,0 +1,44 @@
+// // slow double for
+// function isUnique(str) {
+//     for(let i = 0; i < str.length; i++){
+//         for(let j = i+1; j < str.length; j++){
+//             if(str[i] === str[j]) return false;
+//         }
+//     }
+//     return true;
+// }
+
+// // w/ map
+// function isUnique(str){
+//     let map = new Map();
+//
+//     for(let c in str){
+//         if(map.has(str[c])){
+//             return false;
+//         }else{
+//             map.set(str[c], true)
+//         }
+//     }
+//
+//     return true;
+// }
+
+// // w/ js obj
+function isUnique(str){
+    let map = {};
+
+    for(let c in str){
+        if(map[str[c]]){
+            return false;
+        }else{
+            map[str[c]] =  true;
+        }
+    }
+
+    return true;
+}
+
+console.log(isUnique("jalalbad"));
+console.log(isUnique("bad dog"));
+console.log(isUnique('hi amy'));
+console.log(isUnique('hi amy hi'));
