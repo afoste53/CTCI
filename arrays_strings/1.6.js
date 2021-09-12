76d2febb3bc81f139a011dd34131a0c81c602158
Gabe <mrginch@gmail.com>
2021-09-12 16:36:33 -0500

    add 1.6 - no repeats

---
+++ arrays_strings/1.6.js
@@ -0,0 +1,35 @@
+function compressStr(str) {
+  if (str.length <= 1) {
+    return str;
+  }
+
+  let curr = str[0];
+  let compStr = [...curr];
+  let currCount = 1;
+
+  let i = 1;
+
+  while (i < str.length) {
+    if (str[i] === curr) {
+      currCount++;
+    } else {
+      compStr.push(currCount);
+      if (i + 1 <= str.length) {
+        compStr.push(str[i]);
+        currCount = 1;
+        curr = str[i];
+      }
+    }
+    i++;
+  }
+
+  compStr.push(currCount);
+
+  if (compStr.length >= str.length) {
+    return str;
+  } else {
+    return compStr.join("");
+  }
+}
+
+console.log(compressStr("aaaabbcc"));
