a2b1005c6fe5bd813b7b82db4c6f48d056482091
Gabe <mrginch@gmail.com>
2021-09-20 16:16:09 -0500

    add 1.a

---
+++ arrays_strings/1.a.js
@@ -0,0 +1,43 @@
+/** return the first non-repeating character in a string
+ *  ex. "aaabcccc" -> "b"
+ *      "aaabbcceff" -> "e"
+ *      "aabbcc" -> "_"
+ */
+
+function firstNonRepeat(str) {
+  // if empty string, return "_"
+  if (!str) return "_";
+
+  // create map to remember if we've seen letter before
+  let map = new Map();
+
+  for (let i = 0; i < str.length; i++) {
+    // letter is in map -> set to -1
+    if (map.get(str[i])) {
+      map.set(str[i], -1);
+    } else {
+      // letter is not in map yet -> set to 1
+      map.set(str[i], 1);
+    }
+  }
+
+  // loop through all keys, return the first that is set to true
+  for (let i = 0; i < str.length; i++) {
+    if (map.get(str[i]) === 1) return str[i];
+  }
+
+  // if we've made it this far, there's no hope
+  return "_";
+}
+
+// empty string -> should return "_"
+//console.log(firstNonRepeat(""));
+
+// "aaa" -> "_"
+//console.log(firstNonRepeat("aaa"));
+
+// abbba -> "_"
+//console.log(firstNonRepeat("abba"));
+
+// aabaabcaaa -> "c"
+//console.log(firstNonRepeat("aabaabcaaa"));
